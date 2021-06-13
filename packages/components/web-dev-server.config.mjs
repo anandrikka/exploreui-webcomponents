68d703d1386ea8d5942a319238ccaea882295f39
anandrikka <appsbyanand@gmail.com>
2021-06-13 02:19:46 -0400

    build(@exploreui-webcomponents): removed components package

--- packages/components/web-dev-server.config.mjs
+++
@@ -1,8 +0,0 @@
-const hmr = process.argv.includes('--hmr');
-
-export default /** @type {import('@web/dev-server').DevServerConfig} */ ({
-  nodeResolve: true,
-  open: '/',
-  watch: !hmr,
-  plugins: []
-});
